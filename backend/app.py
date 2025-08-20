d8b53120215b689cc81f929eed36147d4858a5b3
yuvaraju1 <yuvaraju.dasari@talakunchi.com>
2025-08-20 17:39:17 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Backend Home - Try /secret"
+
 @app.route('/secret')
 def secret():
     return "Sensitive backend data!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6000)
+
