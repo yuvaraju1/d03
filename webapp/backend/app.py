dd8bb03f5430c7504b7657d1983b8201f5319733
yuvaraju1 <yuvaraju.dasari@talakunchi.com>
2025-08-20 16:24:16 +0530

    Delete webapp/backend directory

--- webapp/backend/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/secret')
-def secret():
-    return "Sensitive backend data!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6000)
